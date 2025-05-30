d38829d493661865bb52971c399a500a2c7d0b26
Jarvyes14 <javierbarcelosantos@gmail.com>
2025-05-30 10:50:39 -0600

    All entities created

--- src/main/java/com/tecdesoftware/market/persistance/entity/Categoria.java
+++ src/main/java/com/tecdesoftware/market/persistance/entity/Categoria.java
@@ -16,4 +16,27 @@ public class Categoria {
 
     private Boolean estado;
 
+    public Integer getIdCategoria() {
+        return idCategoria;
+    }
+
+    public void setIdCategoria(Integer idCategoria) {
+        this.idCategoria = idCategoria;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+
+    public Boolean getEstado() {
+        return estado;
+    }
+
+    public void setEstado(Boolean estado) {
+        this.estado = estado;
+    }
 }
