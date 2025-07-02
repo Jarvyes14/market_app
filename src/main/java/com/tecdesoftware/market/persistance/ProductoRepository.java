59686d6b8a77199c78844fdb9dd5959bb3cb6241
Jarvyes14 <javierbarcelosantos@gmail.com>
2025-07-02 11:28:48 -0600

    MapStruct implemented

--- src/main/java/com/tecdesoftware/market/persistance/ProductoRepository.java
+++ src/main/java/com/tecdesoftware/market/persistance/ProductoRepository.java
@@ -3,26 +3,36 @@ package com.tecdesoftware.market.persistance;
 import java.util.*;
 import com.tecdesoftware.market.persistance.crud.ProductoCrudRepository;
 import com.tecdesoftware.market.persistance.entity.Producto;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public class ProductoRepository {
 
+    @Autowired
     private ProductoCrudRepository productoCrudRepository;
 
     public List<Producto> getAll() {
-        List<Producto> productos = new ArrayList<>();
-        productoCrudRepository.findAll().forEach(productos::add);
-        return productos;
+        return (List<Producto>)productoCrudRepository.findAll();
     }
 
-    public Optional<Producto> getProducto(int idProducto) {
+    public List<Producto> getByCategoria(int idCategoria) {
+        return productoCrudRepository.findByIdCategoriaOrderByNombreAsc(idCategoria);
+    }
+
+    public Optional<List<Producto>> getEscasos(int cantidad) {
+        return productoCrudRepository.findByCantidadStockLessThanAndEstado(cantidad, true);
+    }
+
+    public Optional<Producto> getProductoById(int idProducto) {
         return productoCrudRepository.findById(idProducto);
     }
 
-    public Producto guardar(Producto producto) {
+    public Producto save(Producto producto) {
         return productoCrudRepository.save(producto);
     }
 
-    public void eliminar(int idProducto) {
-        productoCrudRepository.deleteById(idProducto);
+    public void delete(Producto producto) {
+        productoCrudRepository.delete(producto);
     }
 }
--- src/main/java/com/tecdesoftware/market/persistance/crud/ProductoCrudRepository.java
+++ src/main/java/com/tecdesoftware/market/persistance/crud/ProductoCrudRepository.java
@@ -3,5 +3,12 @@ package com.tecdesoftware.market.persistance.crud;
 import com.tecdesoftware.market.persistance.entity.Producto;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.List;
+import java.util.Optional;
+
 public interface ProductoCrudRepository extends CrudRepository<Producto, Integer> {
+
+    //Query Methods
+    List<Producto> findByIdCategoriaOrderByNombreAsc(int idCategoria);
+    Optional<List<Producto>> findByCantidadStockLessThanAndEstado(int cantidadStock, boolean estado);
 }
